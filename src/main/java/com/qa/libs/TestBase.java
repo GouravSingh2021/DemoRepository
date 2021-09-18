8c3db153bbb5aac412ca128b2b16a3951db6096f
GouravSingh2021 <gouravkrsingh2017@gmail.com>
2021-09-18 21:17:08 +0530

    first git commit for my selenium project

---
+++ src/main/java/com/qa/libs/TestBase.java
@@ -0,0 +1,11 @@
+package com.qa.libs;
+
+public class TestBase {
+	
+	
+	
+	public void base() {
+		System.out.println("baseclass");
+	}
+
+}
---
+++ src/main/java/com/qa/util/TestUtil.java
@@ -0,0 +1,13 @@
+package com.qa.util;
+
+public class TestUtil {
+	
+	public int testutil(int a, int b) {
+		System.out.println("testutil");
+		int c = a+b;
+		return c;
+		
+		
+	}
+
+}
