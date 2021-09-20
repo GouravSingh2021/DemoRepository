f863600ee07a7fedab56bf3cca522d8655ae20c4
GouravSingh2021 <gouravkrsingh2017@gmail.com>
2021-09-20 20:38:05 +0530

    newcommit for changes

---
+++ src/main/java/com/qa/pages/AddingNewClass.java
@@ -0,0 +1,14 @@
+package com.qa.pages;
+
+import org.testng.annotations.Test;
+
+public class AddingNewClass {
+
+	
+	@Test
+	public void newclass() {
+		
+		System.out.println("New Class");
+		
+	}
+}
